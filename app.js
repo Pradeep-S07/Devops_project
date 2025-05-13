aa0a5a6c0b566fbb6028903c90da5962a4a52db5
Pradeep S <144413504+Pradeep-S07@users.noreply.github.com>
2025-05-13 08:10:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Route to serve index.html
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
